a891ed7438ff9fc648571e9294c347bcf3900a48
Samuel Jesus <samueljesus@live.com>
2022-08-04 09:40:53 -0300

    Initial Commit

---
+++ Movies/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Movies
+//
+//  Created by Samuel Nascimento de Jesus on 04/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Samuel Nascimento de Jesus on 04/08/22.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
